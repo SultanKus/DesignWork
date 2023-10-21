e8469822dbcf177bbb7a10ab4dc9385bf307f6be
SultanKus <kussultannn34@gmail.com>
2023-10-21 17:02:38 +0300

    Initial commit

---
+++ app/src/main/java/com/example/designwork/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.designwork
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //yazılım kısmında R harfi res klasörünü temsil eder.
+        //xml kısmında @ işareti res klasörünü temsil eder.
+        //Left - Start - Leading
+        //Right - End -  Trailing
+
+    }
+}
